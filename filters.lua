c7259994cd79c7bdb44e0e77e6b24aa57633638e
777shuang <shuang777@outlook.jp>
2024-03-17 19:35:29 +0900

    Load filter

--- filters.lua
+++ filters.lua
@@ -1,5 +1,6 @@
 local filters = require "pandoc-include.pandoc-include"
 filters = pandoc.List(filters):__concat(require "lua-filters.include-code-files.include-code-files")
 filters = pandoc.List(filters):__concat(require "lua-filters.diagram-generator.diagram-generator")
+filters = pandoc.List(filters):__concat(require "pandoc-obsidian-filter.obsidian")
 filters = pandoc.List(filters):__concat(require "fenced_div")
 return filters
