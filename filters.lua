9ec314aab6f81d5d9373841e1417b652cb96483d
777shuang <shuang777@outlook.jp>
2024-04-05 03:23:52 +0900

    フィルターを読み込む

--- filters.lua
+++ filters.lua
@@ -2,5 +2,6 @@ local filters = require "pandoc-include.pandoc-include"
 filters = pandoc.List(filters):__concat(require "lua-filters.include-code-files.include-code-files")
 filters = pandoc.List(filters):__concat(require "lua-filters.diagram-generator.diagram-generator")
 filters = pandoc.List(filters):__concat(require "pandoc-obsidian-filter.obsidian")
+filters = pandoc.List(filters):__concat(require "sky-y-lua-filters.filters.tcolorbox-column")
 filters = pandoc.List(filters):__concat(require "fenced_div")
 return filters
