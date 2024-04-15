f1eaca64f2aa3c28c85f6850a1cd6a6ea1c9e307
777shuang <100687590+777shuang@users.noreply.github.com>
2024-04-16 03:07:22 +0900

    Update fenced_div.lua

--- fenced_div.lua
+++ fenced_div.lua
@@ -1,6 +1,6 @@
 if FORMAT:match 'latex' then
     function Div(el)
-        if #el.classes == 1 then
+        if #el.classes == 1 and el.classes[1] != "box" and el.classes[1] != "column" then
             -- insert element in front
             table.insert(el.content, 1, pandoc.RawBlock("latex", "\\begin{" .. el.classes[1] .. "}"))
             -- insert element at the back
